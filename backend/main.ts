faa0302c02057c1cbe174eb57eb9809e08a0c5d0
David Song <david.jh.song@gmail.com>
2025-11-26 17:54:42 +0900

    fix: esModuleInterop fixed

--- backend/main.ts
+++ backend/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
-import * as cookieParser from "cookie-parser";
+import cookieParser from "cookie-parser";
 
 // Add BigInt serialization support
 (BigInt.prototype as any).toJSON = function () {
