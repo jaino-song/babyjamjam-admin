b484c49f0a65c692628bd020ad9e52d67c4b8676
David Song <david.jh.song@gmail.com>
2025-11-27 01:52:53 +0900

    login button fixed

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,15 +3,10 @@ import { Box, Button, Typography } from "@mui/material";
 
 
 export default async function Home() {
-  const API_BASE_URL =
-  process.env.RAILWAY_PUBLIC_API_BASE_URL ||
-    process.env.PRODUCTION_API_BASE_URL ||
-    process.env.DEVELOPMENT_API_BASE_URL;
-
   return (
     <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", height: "100vh" }}>
       <Typography variant="h3" fontWeight={700} sx={{ mb: 2 }}>인천 아이미래로 백오피스</Typography>
-      <Link href={`${API_BASE_URL}/auth/kakao`} style={{ textDecoration: "none" }}>
+      <Link href={`/login`} style={{ textDecoration: "none" }}>
         <Button variant="contained" color="primary">시작하기</Button>
       </Link>
     </Box>
